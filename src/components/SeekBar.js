74cc9f82dc3ca459e14868290d007e502252e7b8
Kieran C <kieranakc2@gmail.com>
2021-10-25 19:10:14 +0100

    small performance changes. memoized functions

--- src/components/SeekBar.js
+++ src/components/SeekBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 
 // let isMouseDown = false;
 // let mouseMoveSeekDelay = false;
@@ -13,6 +13,55 @@ export default function SeekBar(props) {
   const mouseMoveSeekDelay = useRef(false);
   const mouseUpSeekDelay = useRef(false);
 
+  const handleMouseUp = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (isMouseDown.current) {
+        setSeeking((isSeeking) => !isSeeking);
+        isMouseDown.current = false;
+        mouseMoveSeekDelay.current = false;
+        audioRef.current.muted = false;
+        if (!mouseUpSeekDelay.current) {
+          seekBarScrub(e.clientX, seekBarRef.current.offsetWidth);
+        }
+      }
+    },
+    [audioRef, seekBarScrub]
+  );
+
+  const handleMouseMove = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (isMouseDown.current && mouseMoveSeekDelay.current) {
+        audioRef.current.muted = true;
+        setTimeout(() => {
+          mouseMoveSeekDelay.current = true;
+          seekBarScrub(e.clientX, seekBarRef.current.offsetWidth);
+        }, 60);
+        mouseMoveSeekDelay.current = false;
+      }
+    },
+    [audioRef, seekBarScrub]
+  );
+
+  const handleMouseDown = useCallback(
+    (e) => {
+      if (e.buttons === 1) {
+        e.preventDefault();
+        setSeeking((userIsSeeking) => !userIsSeeking);
+        isMouseDown.current = true;
+        mouseMoveSeekDelay.current = true;
+        seekBarScrub(e.clientX, seekBarRef.current.offsetWidth);
+        mouseUpSeekDelay.current = true;
+        mouseUpSeekDelayTimeoutRef.current = setTimeout(
+          () => (mouseUpSeekDelay.current = false),
+          100
+        );
+      }
+    },
+    [seekBarScrub]
+  );
+
   useEffect(() => {
     document.body.addEventListener('mouseup', handleMouseUp);
     document.body.addEventListener('mousemove', handleMouseMove);
@@ -21,47 +70,7 @@ export default function SeekBar(props) {
       document.body.removeEventListener('mouseup', handleMouseUp);
       document.body.removeEventListener('mousemove', handleMouseMove);
     };
-  });
-
-  const handleMouseUp = (e) => {
-    e.preventDefault();
-    if (isMouseDown.current) {
-      setSeeking((isSeeking) => !isSeeking);
-      isMouseDown.current = false;
-      mouseMoveSeekDelay.current = false;
-      audioRef.current.muted = false;
-      if (!mouseUpSeekDelay.current) {
-        seekBarScrub(e.clientX, seekBarRef.current.offsetWidth);
-      }
-    }
-  };
-
-  const handleMouseMove = (e) => {
-    e.preventDefault();
-    if (isMouseDown.current && mouseMoveSeekDelay.current) {
-      audioRef.current.muted = true;
-      setTimeout(() => {
-        mouseMoveSeekDelay.current = true;
-        seekBarScrub(e.clientX, seekBarRef.current.offsetWidth);
-      }, 60);
-      mouseMoveSeekDelay.current = false;
-    }
-  };
-
-  const handleMouseDown = (e) => {
-    if (e.buttons === 1) {
-      e.preventDefault();
-      setSeeking((userIsSeeking) => !userIsSeeking);
-      isMouseDown.current = true;
-      mouseMoveSeekDelay.current = true;
-      seekBarScrub(e.clientX, seekBarRef.current.offsetWidth);
-      mouseUpSeekDelay.current = true;
-      mouseUpSeekDelayTimeoutRef.current = setTimeout(
-        () => (mouseUpSeekDelay.current = false),
-        100
-      );
-    }
-  };
+  }, [handleMouseMove, handleMouseUp]);
 
   return (
     <div ref={seekBarRef} className="seek-bar-wrapper" onMouseDown={handleMouseDown}>
